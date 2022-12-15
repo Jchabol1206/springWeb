34aefb7293389a735557ebea770c4ccbd58cf860
usuario <usuario@DAW2ALU20>
2022-12-15 08:58:44 +0100

    actividad 2 completa

---
+++ src/main/java/org/iesvdm/controlador/ComercialController.java
@@ -0,0 +1,32 @@
+package org.iesvdm.controlador;
+
+import java.util.List;
+
+import org.iesvdm.modelo.Comercial;
+import org.iesvdm.service.ComercialService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class ComercialController {
+	
+	private ComercialService comercialService;
+
+	public ComercialController(ComercialService comercialService) {
+		this.comercialService = comercialService;
+	}
+	
+	
+	
+	@GetMapping("/comerciales")
+	public String listar(Model model) {
+		
+		List<Comercial> listaComerciales = comercialService.listAll();
+		model.addAttribute("listaComerciales", listaComerciales);
+		
+		return "comerciales";
+		
+		
+	}
+}
